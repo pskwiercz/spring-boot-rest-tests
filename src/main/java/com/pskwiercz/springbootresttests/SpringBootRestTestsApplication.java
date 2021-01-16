45b5c2d5226d8c401942ba6d2d52f4682581d52f
Przemek Skwiercz <przemek.skwiercz@gmail.com>
2021-01-16 19:57:02 +0100

    Initial commit

---
+++ src/main/java/com/pskwiercz/springbootresttests/SpringBootRestTestsApplication.java
@@ -0,0 +1,13 @@
+package com.pskwiercz.springbootresttests;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestTestsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootRestTestsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pskwiercz/springbootresttests/SpringBootRestTestsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pskwiercz.springbootresttests;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRestTestsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
